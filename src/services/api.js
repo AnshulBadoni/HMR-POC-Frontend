b20d36697092dadcc83d447035dcc11dce54435f
AnshulBadoni <anshulbadoni359@gmail.com>
2026-02-06 00:14:09 +0530

    Remove bugs

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = "https://hmr-poc.onrender.com";
 
 const api = axios.create({
-  baseURL: `${API_BASE_URL}`,
+  baseURL: `${API_BASE_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
